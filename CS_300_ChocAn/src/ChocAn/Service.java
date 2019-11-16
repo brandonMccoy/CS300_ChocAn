af69c0daeaae37fb67ca3edb67100ed88c871e3a
Brandon McCoy <hendrix7@student.clackamas.edu>
2019-11-16 10:58:29 -0800

    Update Service.java

--- CS_300_ChocAn/src/ChocAn/Service.java
+++ CS_300_ChocAn/src/ChocAn/Service.java
@@ -6,7 +6,14 @@ public class Service implements Model {
 	
 	public int code;
 	public String name;
-	
+
+	/**Add, Delete, or Update this Service in the database. Or check to see if 
+	 * this Service is already in the database.
+	 * @param action tells the method whether to add, delete, update, or check 
+	 * if the Service is in the database.
+	 * @return Depending on the argument, returns true if Service is 
+	 * successfully added, deleted, updated, or found in the Service database.
+	 */
 	@Override
 	public Boolean Database(Action action) {
 		Boolean response = false;
@@ -21,25 +28,43 @@ public class Service implements Model {
 		}
 		return response;
 	}
-	
+
+	/**Searches the Service database for an object with a matching id.
+	 * @param id of Service to find in the database
+	 * @return A copy of the Service in the database that matches the id arg. 
+	 * If a match is not found, an empty Service is returned with id = -1.
+	 */
 	@Override
 	public Service Get(int code) {
 		return db.GetService(code);
 	}
 	
-	/// Returns an unmodifiable list of all members in the memberDB database
+	/**
+	 * @return An unmodifiable list of all members in the memberDB database
+	 */
 	public List<Service> GetAll(){
 		return db.AllServices();
 	}
-	
+
+	/**
+	 * @param code is the service code linked to the service name to return.
+	 * @return The name of the service that matches the code argument.
+	 */
 	public String GetServiceName(int code) {
 		return db.GetServiceName(code);
 	}
-	
+
+	/**
+	 * @param serviceName of the service that is linked to the service code to return.
+	 * @return service code that matches the service name argument.
+	 */
 	public int GetServiceCode(String serviceName) {
 		return db.GetServiceCode(serviceName);
 	}
-	
+
+	/**Prints all Service variables to the console, 
+	 * formatting specific to a Service type.
+	 */
 	@Override
 	public void Print() {
 		String message = "Name: " + name;
