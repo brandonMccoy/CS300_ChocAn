02d5877329ef42aa5651ccc2820f4dfb9e431951
Brandon McCoy <10971953+brandonMccoy@users.noreply.github.com>
2019-12-02 19:44:15 -0800

    Update UserModelTest.java

--- CS_300_ChocAn/tests/ChocAn/UserModelTest.java
+++ CS_300_ChocAn/tests/ChocAn/UserModelTest.java
@@ -1,14 +1,38 @@
 package ChocAn;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class UserModelTest {
 
+	UserModel testUserModel;
+	
+	@BeforeEach
+	void setUp(){
+		testUserModel = new UserModel();
+		testUserModel.id = 123456;
+		testUserModel.name = "name";
+		testUserModel.street = "street";
+		testUserModel.city = "city";
+		testUserModel.state = "state";
+		testUserModel.zip = "zip";
+	}
+	
+	@AfterEach
+	void tearDown(){
+		testUserModel = null;
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testConstructor() {
+		UserModel newUserModel = new UserModel();
+		asserNotNull("UserModel constructor failed",newUserModel);
 	}
 
 }
