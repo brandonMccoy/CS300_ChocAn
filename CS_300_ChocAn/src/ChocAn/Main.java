e2f409d8bc2cbcf9476a6b2ce8e198e2df5b87be
Brandon McCoy <10971953+brandonMccoy@users.noreply.github.com>
2019-12-06 20:54:55 -0800

    Cleaned out main except for View.Start()

--- CS_300_ChocAn/src/ChocAn/Main.java
+++ CS_300_ChocAn/src/ChocAn/Main.java
@@ -1,25 +1,7 @@
 package ChocAn;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
-import ChocAn.IModel.Action;
-
 public class Main {
-
 	public static void main(String[] args) {
-		
-		//View.TerminalChoice();
-		
-		Member member = new Member();
-		Provider provider = new Provider();
-		Consultation consul = new Consultation();
-		Service service = new Service();
-		//database filled upon creation of new object
-		Database database = new Database();
-		
-		//need to call WriteAllToDisk() after user wants to quit but before program exits
-		database.WriteAllToDisk();
+		View.Start();
 	}
-
 }
